bb0b859d287072779ee48b97dc612b930e62789d
MikiKru <michal_kruczkowski@o2.pl>
2021-03-06 15:39:11 +0100

    dodanie klasy User

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("START");
+        User u1 = new User("mk@mk.pl","mk");
+        User u2 = new User("ll@ll.pl","ll");
+        System.out.println(u1);
+        System.out.println(u2);
     }
 }
---
+++ src/User.java
@@ -0,0 +1,17 @@
+// ALT + Ins -> generate
+public class User {
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
